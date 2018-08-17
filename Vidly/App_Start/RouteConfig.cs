0cde2a718cadb8663d14d03580831ff0e676f854
ankitakhurana <ankitakhurana@ANKITA3148515>
2018-08-17 11:34:36 +0530

    Vidly App Video Part

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -7,11 +7,21 @@ using System.Web.Routing;
 
 namespace Vidly
 {
+
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+
+
+            //routes.MapRoute(
+            //      "MoviesByReleaseDate",
+            //       "Movies/release/{year}/{month}",
+            //        new { controller = "Movies", action = "OrderByDate" },
+            //        new { year = @"\d{4}", month = @"\d{2}" }
+            //);
 
             routes.MapRoute(
                 name: "Default",
